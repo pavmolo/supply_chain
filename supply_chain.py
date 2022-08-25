5471bde9583cdf57bbaff97e4bd060bada295940
pavmolo <74321783+pavmolo@users.noreply.github.com>
2022-08-25 14:09:33 +0300

    Update supply_chain.py

--- supply_chain.py
+++ supply_chain.py
@@ -3,3 +3,6 @@ import numpy as np
 import pandas as pd
 import plotly.graph_objects as go
 import plotly.express as px
+
+st.subheader("Заполните поля ниже:")
+current_stock = st.number_input(f"Установите текущий уровень запаса в штуках", value=0)
