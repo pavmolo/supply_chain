3f2c33c7513fc63d0e05f2acd5c275a4955e5c21
pavmolo <74321783+pavmolo@users.noreply.github.com>
2022-08-26 21:14:55 +0300

    Update supply_chain.py

--- supply_chain.py
+++ supply_chain.py
@@ -114,12 +114,12 @@ st.info(f"Страхового запаса не зватило в {quant_defici
 
 fig = go.Figure()
 fig.add_trace(go.Scatter(x=df.index, y=df['stocks'], fill='tozeroy', name='Запас перед поставкой'))
-#fig.add_trace(go.Scatter(x=df.index, y=df['stock_level_before_replenishment'], fill='tozeroy', name='Уровень запаса перед пополнением'))
+fig.add_trace(go.Scatter(x=df.index, y=df['stock_level_before_replenishment'], fill='tozeroy', name='Уровень запаса перед пополнением'))
 fig.add_trace(go.Scatter(x=df.index, y=df['safety_stocks'], name='Страховой запас'))
 fig.add_trace(go.Scatter(x=df.index, y=df['reorder_level'], name='Точка заказа'))
 
 st.plotly_chart(fig, use_container_width=False, sharing="streamlit")
 
 
-#st.table(data=df)
+st.table(data=df)
 
