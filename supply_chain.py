457290bf4744fd46bb95e1cc2457e2bc28d1e5bc
pavmolo <74321783+pavmolo@users.noreply.github.com>
2024-03-11 14:31:34 +0300

    Update supply_chain.py

--- supply_chain.py
+++ supply_chain.py
@@ -5,7 +5,7 @@ import plotly.graph_objects as go
 import plotly.express as px
 import random
 
-st.markdown('''<p>Разработчик: Павел Молотивский<p>''', unsafe_allow_html=True)
+st.markdown('Разработчик: Павел Молотивский')
 
 with st.sidebar:
   st.title('Введите данные для моделирования элемента цепочки поставок (конкретного SKU)')
